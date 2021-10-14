13cd32b8a3a9bc4bc680e0348b38928b563997d3
GMMarshall <gmmarshall27@gmail.com>
2021-10-14 19:09:11 -0400

    added server.js and started notes.js

---
+++ routes/notes.js
@@ -0,0 +1,15 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../fsUtils');
+
+// GET Route for retrieving all the tips
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+notes.post('/', (req,res) => {
+    
+})
